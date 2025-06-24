09400129d0316bac39d624da2f3b468fc79f5e88
Ali Fadaei <alifadaei_123@yahoo.com>
2025-06-24 14:46:34 +0330

    fix: resolve translation display issue

--- src/resources/views/livewire/auth-password.blade.php
+++ src/resources/views/livewire/auth-password.blade.php
@@ -1,27 +1,28 @@
 <div class="form-box">
     <section class="form-box-content">
         <header class="logo-box">
-            <img src="{{ asset('assets/img/tmp/tmp-full-logo.png') }}" alt="{{ trans('app.logo_alt') }}">
+            <img src="{{ config('manager.logo_path')??asset('assets/vendor/manager/img/logo.png') }}"
+                 alt="{{ trans('app.logo_alt') }}">
         </header>
 
         <div class="form-body">
             <div class="form-box-main">
-                <h1>{{ trans('auth.welcome_back') }}</h1>
-                <p>{{ trans('auth.steps.security.box_sub_title') }}</p>
+                <h1>{{ trans('manager::manager.welcome_back') }}</h1>
+                <p>{{ trans('manager::manager.steps.security.box_sub_title') }}</p>
 
                 <form wire:submit.prevent="submit" novalidate>
                     <div class="input-box @error('token') error @enderror">
-                        <p>{{ trans('auth.labels.password') }}</p>
+                        <p>{{ trans('manager::manager.labels.token') }}</p>
                         <div class="form-input">
                             <input type="password" wire:model="token"
-                                   placeholder="{{ trans('auth.steps.security.token_placeholder') }}">
+                                   placeholder="{{ trans('manager::manager.steps.security.token_placeholder') }}">
                             <i onclick="AuthScripts.showHidePass(this)" class="icon-password"></i>
                         </div>
                         <span data-message="{{$errors->first('token')}}"></span>
                     </div>
 
                     <button type="submit" class="btn-submit-form">
-                        {{ trans('auth.confirm') }}
+                        {{ trans('manager::manager.confirm') }}
                     </button>
                 </form>
             </div>
