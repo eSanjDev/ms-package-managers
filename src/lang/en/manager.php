590a46fff8e535c5ee4a68fb38bfac48dcf20a68
Ali Fadaei <alifadaei_123@yahoo.com>
2025-06-24 14:42:03 +0330

    feat: update language files

--- src/lang/en/manager.php
+++ src/lang/en/manager.php
@@ -1,5 +1,28 @@
 <?php
 
 return [
-    'title' => 'Enter Token'
+    'title' => 'Enter Token',
+
+    'labels' => [
+        'token' => 'Enter Token',
+    ],
+
+    'welcome_back' => 'Welcome back',
+    'confirm' => 'Confirm',
+    'cancel' => 'Cancel',
+
+    'steps' => [
+        'security' => [
+            'box_sub_title' => 'To log in, please enter your account’s static token.',
+            'token_placeholder' => 'Please enter your token',
+        ],
+    ],
+
+    'errors' => [
+        'token_not_found' => 'Token not found',
+        'token_incorrect' => 'Token is incorrect',
+        'public_key_not_found' => 'Public key not found',
+        'invalid_token' => 'Invalid token',
+        'too_many_attempts' => 'Too many attempts, please try again later',
+    ],
 ];
