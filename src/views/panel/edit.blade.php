8df0d50ed4c9c41c8b68d689433f28196b0ffcb2
Ali Fadaei <alifadaei_123@yahoo.com>
2025-07-21 19:05:24 +0330

    fix: resolve select input issues

--- src/views/panel/edit.blade.php
+++ src/views/panel/edit.blade.php
@@ -89,9 +89,9 @@
                     <div class="col-12 position-relative select-box mb-4">
                         <div class="form-floating">
                             <select name="is_active" class="form-select">
-                                <option @selected(old('is_active', $manager->is_active) == "1") value="1">Active
+                                <option @selected(old('is_active', $manager->is_active)) value="1">Active
                                 </option>
-                                <option @selected(old('is_active', $manager->is_active) == "0")value="0">diactive
+                                <option @selected(!old('is_active', $manager->is_active)) value="0">diactive
                                 </option>
                             </select>
                             <label>Status</label>
@@ -100,9 +100,9 @@
                     <div class="col-12 position-relative select-box mb-4">
                         <div class="form-floating">
                             <select name="api_access" class="form-select">
-                                <option @selected(old('api_access', $manager->api_access) === 1) value="1">Yes
+                                <option @selected(old('api_access', $manager->api_access)) value="1">Yes
                                 </option>
-                                <option @selected(old('api_access', $manager->api_access) === 0) value="0">No
+                                <option @selected(!old('api_access', $manager->api_access)) value="0">No
                                 </option>
                             </select>
                             <label>Api access</label>
